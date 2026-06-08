73004985b096fed788d2a9d40ebff33c81e38e64
david <davidklobo2006@gmail.com>
2026-06-08 20:09:53 +0200

    new5

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
     //why are they trying to embarass her like that because parents be like thhat
+    //why are they trying to embarass her like that fr fr
 
 
     // yeah right
