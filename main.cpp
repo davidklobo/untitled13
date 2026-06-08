b2bfcd2cc44a2b28bcb3d11dd2b9c7c88441a11a
daria <dariaakana@gmail.com>
2026-06-08 20:09:23 +0200

    DAR $

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
-    //why are they trying to embarass her like that
+    //why are they trying to embarass her like that because parents be like thhat
 
 
     // yeah right
